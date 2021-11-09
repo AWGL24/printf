165d2790797a6ced5d3821220b03cff79fa6560c
Drew-24 <3239@holbertonschool.com>
2021-11-09 18:52:34 +0000

    added caseswitch file

---
+++ caseswitch.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * caseswitch - function that select wich function we need
+ * @list: variadical arguments from printf
+ * @ch: character
+ * Return: lenght of what is printed
+ */
+int caseswitch(char ch, va_list list)
+{
+	int hold = 0, idx;
+
+	mod x[] = {
+		{'c', printchar},
+		{'s', printstr},
+		{'i', printnum},
+		{'d', printnum},
+		{'\0', NULL},
+	};
+
+	for (idx = 0; x[idx].l != '\0'; idx++)
+	{
+		if (x[idx].l == ch)
+		{
+			hold += x[idx].f(list);
+			return (hold);
+		}
+	}
+	hold += _putchar('%');
+	hold += _putchar(ch);
+	return (hold);
+}
