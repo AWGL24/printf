f32d5f783b7d450dc85ce122ec5af39bce7aec16
Drew-24 <3239@holbertonschool.com>
2021-11-09 17:17:58 +0000

    _print main file

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - function that emulates a real printf
+ * @format: string that looks for extra arguments
+ * Return: len
+ */
+int _printf(const char *format, ...)
+{
+	int idx, len = 0;
+	va_list list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	if (list == NULL)
+		return (-1);
+
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+
+	for (idx = 0; format[idx] != '\0'; idx++)
+	{
+		if (format[idx] == '%' && format[idx + 1] != '\0')
+		{
+			if (format[idx + 1] == '%')
+			{
+				len += _putchar(format[idx++]);
+			}
+			else
+			{
+				len += caseswitch(format[idx++], list);
+			}
+		}
+		else
+		{
+			if (format[idx] == '%' && format[idx + 1] == '\0')
+				return (-1);
+			len += _putchar(format[idx]);
+		}
+	}
+	va_end(list);
+	return (len);
+}
